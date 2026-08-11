d841d262616fd4a526b011c7e4bb04b3a26845cb
Su-57 <omarchyhyprland@gmail.com>
2026-08-11 23:23:18 +0530

    chore: refresh PWA cache for animation fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'modern-calculator-v56';
+const CACHE_NAME = 'modern-calculator-v57';
 const APP_SHELL = ['./','./index.html','./styles.css','./script.js','./calculator-core-ui.js','./packages/calculator-core/index.js','./manifest.json'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
