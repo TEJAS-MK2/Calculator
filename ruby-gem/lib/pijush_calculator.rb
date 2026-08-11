8389c0203e9d17a74b15875e6dd9919c1ef348c7
Su-57 <omarchyhyprland@gmail.com>
2026-08-11 12:09:28 +0530

    Add Ruby calculator gem core

---
+++ ruby-gem/lib/pijush_calculator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module PijushCalculator
+  VERSION = "0.1.0"
+
+  module_function
+
+  def add(a, b) = a + b
+  def subtract(a, b) = a - b
+  def multiply(a, b) = a * b
+  def divide(a, b)
+    raise ZeroDivisionError, "cannot divide by zero" if b == 0
+
+    a.to_f / b
+  end
+end
