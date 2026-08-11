d456aa85d9b75f9228f38d05659b106c2006c1e2
Su-57 <omarchyhyprland@gmail.com>
2026-08-11 20:14:41 +0530

    feat(ruby): update calculator engine

--- ruby-gem/lib/pijush_calculator.rb
+++ ruby-gem/lib/pijush_calculator.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
 module PijushCalculator
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 
   module_function
 
   def add(a, b) = a + b
   def subtract(a, b) = a - b
   def multiply(a, b) = a * b
+
   def divide(a, b)
     raise ZeroDivisionError, "cannot divide by zero" if b == 0
 
     a.to_f / b
   end
+
+  def modulo(a, b)
+    raise ZeroDivisionError, "cannot modulo by zero" if b == 0
+
+    a % b
+  end
+
+  def power(a, b) = a**b
+  def percentage(value, percent) = value * percent / 100.0
 end
