d3cb5541138a70eecc39b288e0e391f7c7e21f6a
Su-57 <omarchyhyprland@gmail.com>
2026-08-11 20:26:43 +0530

    engine: expand Ruby arithmetic API

--- ruby-gem/lib/pijush_calculator.rb
+++ ruby-gem/lib/pijush_calculator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module PijushCalculator
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
 
   module_function
 
@@ -11,16 +11,31 @@ module PijushCalculator
 
   def divide(a, b)
     raise ZeroDivisionError, "cannot divide by zero" if b == 0
-
     a.to_f / b
   end
 
   def modulo(a, b)
     raise ZeroDivisionError, "cannot modulo by zero" if b == 0
-
     a % b
   end
 
   def power(a, b) = a**b
   def percentage(value, percent) = value * percent / 100.0
+  def absolute(value) = value.abs
+  def minimum(a, b) = [a, b].min
+  def maximum(a, b) = [a, b].max
+  def average(a, b) = (a + b) / 2.0
+
+  def clamp(value, minimum_value, maximum_value)
+    raise ArgumentError, "minimum_value cannot exceed maximum_value" if minimum_value > maximum_value
+    [[value, minimum_value].max, maximum_value].min
+  end
+
+  def reciprocal(value)
+    raise ZeroDivisionError, "cannot take reciprocal of zero" if value == 0
+    1.0 / value
+  end
+
+  def square(value) = value * value
+  def cube(value) = value * value * value
 end
