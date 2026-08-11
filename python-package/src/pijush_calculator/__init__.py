239c8d6d09c32c412361ca7f0e58259b22e7e344
Su-57 <omarchyhyprland@gmail.com>
2026-08-11 14:21:38 +0530

    release: bump Python package version to 0.1.1

--- python-package/src/pijush_calculator/__init__.py
+++ python-package/src/pijush_calculator/__init__.py
@@ -1,6 +1,6 @@
-"""Lightweight calculator engine."""
+"""Lightweight, dependency-free calculator engine."""
 
 from .calculator import add, subtract, multiply, divide
 
 __all__ = ["add", "subtract", "multiply", "divide"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
