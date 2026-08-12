2ce924de0c3f93e374925d4be634fb01c4cedcf7
Su-57 <omarchyhyprland@gmail.com>
2026-08-12 20:48:27 +0530

    test: add shared conformance vectors to Ruby

--- ruby-gem/test/test_pijush_calculator.rb
+++ ruby-gem/test/test_pijush_calculator.rb
@@ -27,4 +27,20 @@ class PijushCalculatorTest < Minitest::Test
   def test_version
     refute_empty PijushCalculator::VERSION
   end
+
+  def test_shared_conformance_vectors
+    vectors = [
+      [:add, 12.5, 7.5, 20.0],
+      [:subtract, 12.5, 7.5, 5.0],
+      [:multiply, 12.5, 8.0, 100.0],
+      [:divide, 12.5, 2.5, 5.0],
+      [:add, -7.0, 2.0, -5.0],
+      [:multiply, -3.0, -4.0, 12.0]
+    ]
+
+    vectors.each do |operation, a, b, expected|
+      actual = PijushCalculator.public_send(operation, a, b)
+      assert_in_delta expected, actual, 1e-12, "#{operation}(#{a}, #{b})"
+    end
+  end
 end
